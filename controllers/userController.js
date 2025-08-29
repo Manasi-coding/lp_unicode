bbd4f9df27f49b1c36bcf4f9b51ef7ff3ad53127
Manasi S <manasimvs11@gmail.com>
2025-08-30 01:47:26 +0530

    Included the basic CRUD operations

---
+++ controllers/userController.js
@@ -0,0 +1,68 @@
+import User from '../models/userModel.js';
+
+// CREATE USER
+export const createUser = async (req,res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    }
+    catch (err) {
+        res.status(400).json({message: err.message});
+    }
+};
+
+// GET ALL THE USERS
+export const getUsers = async (req,res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    }
+    catch (err) {
+        res.status(400).json({message: err.message});
+    }
+};
+
+// GET A SINGLE USER
+export const getUser = async (req,res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) {
+            return res.status(404).json({message: 'User not found!'});
+        }
+        res.json(user);
+    }
+    catch (err) {
+        res.status(400).json({message: err.message});
+    }
+};
+
+// UPDATE A USER
+export const updateUser = async (req,res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        })
+        if (!user) {
+            return res.status(404).json({message: 'User not found!'});
+        }
+        res.json(user);
+    }
+    catch (err) {
+        res.status(400).json({message: err.message});
+    }
+};
+
+// DELETE A USER
+export const deleteUser = async (req,res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({message: 'User not found!'});
+        }
+        res.json({message: 'User successfully deleted!'});
+    }
+    catch (err) {
+        res.status(400).json({message: err.message});
+    }
+};
