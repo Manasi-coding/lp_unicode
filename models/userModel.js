85ad9bbf1eab29b934556b8710b76d4f1b91f13d
Manasi S <manasimvs11@gmail.com>
2025-08-30 01:22:39 +0530

    Configured to MongoDB & written the user schema

---
+++ models/userModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema (
+    {
+        name: {type: String, required: true},
+        email: {type: String, required: true, unique: true},
+        dob: {type: String, required: true},
+        credit_scores: {type: Number, required: true},
+    },
+    {timestamps: true}
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
