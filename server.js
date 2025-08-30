fdbe2b870747e7097a7eecbf38fb3ce014266358
Manasi S <manasimvs11@gmail.com>
2025-08-30 20:20:12 +0530

    Completed server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import connectDB from "./config/db";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req,res) => {
+    res.send("THE API IS NOW RUNNING!");
+});
+
+const PORT = process.env.PORT || 5000;
+connectDB();
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
