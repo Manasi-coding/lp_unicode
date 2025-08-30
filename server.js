496c95537a6041c9dac1f6afc6e585ea88d4b8d5
Manasi S <manasimvs11@gmail.com>
2025-08-30 23:30:07 +0530

    Errors resolved

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.get("/", (req,res) => {
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-app.listen(PORT, () => console.log(`Server running on http://localhost :${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
